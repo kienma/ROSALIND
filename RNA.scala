cae7064a02d246c4ba4e847ef5c0e0cf645767c1
Kien Malarney <malarneykien@gmail.com>
2017-04-16 16:47:30 +0900

    Update RNA.scala

--- RNA.scala
+++ RNA.scala
@@ -1,11 +1,4 @@
-import scala.io.Source
-
 class RNA (seq: String){
-  // reads the text file as a sequence  
-  def sequenceFile(file:String):String={
-    val sequence = scala.io.Source.fromFile(file).mkString.capitalize
-    return sequence
-  }
   // transcribes DNA into RNA 
   def transcribe(sequence:String):String={ 
     return sequence.replace('T', 'U')
