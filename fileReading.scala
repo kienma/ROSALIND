2a99bdc74fed49333f1ad0509627165d9dc5171c
Kien Malarney <malarneykien@gmail.com>
2017-04-16 17:31:19 +0900

    Update fileReading.scala

--- fileReading.scala
+++ fileReading.scala
@@ -1,22 +1,24 @@
 import scala.io.Source
 
 class fileReading {
+  
   // reads the txt file as a sequence  
-  def txt_one_sequence(file:String):String={
+  def txt_sequence1(file:String):String={
     val sequence = scala.io.Source.fromFile(file).mkString.capitalize
     val final_sequence = sequence.filter(i => i != '\n')
     return final_sequence
   }
-  // reads txt file for many sequences 
+  
+  // reads txt file for many lines 
   def txt_sequences(file:String):Vector[String]={
     val raw = scala.io.Source.fromFile(file).mkString.capitalize
-    val RawSequences = raw.split('\n')
-    var sequences:Vector[String] = Vector()
-    for (i <- 0 to RawSequences.length-1){
-      val final_sequence = RawSequences(i).filter(i => i != '\n')
-      sequences = sequences :+ final_sequence
+    val RawLines = raw.split('\n')
+    var lines:Vector[String] = Vector()
+    for (i <- 0 to RawLines.length-1){
+      val fin_line = RawLines(i).filter(i => i != '\n') // processed line, without the indent 
+      lines = lines :+ fin_line
     }
-    return sequences
+    return lines
   }
   
 }
